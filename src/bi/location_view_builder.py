7e3de223b47c75c30263059ab341e07dff40721d
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-10-30 13:22:21 +0900

    feat: location_view 생성 시 예외처리 추가

--- src/bi/location_view_builder.py
+++ src/bi/location_view_builder.py
@@ -1,24 +1,22 @@
 from src.common.redshift_client import RedShiftClient
 from src.common.query_loader import load_query
 
-client = RedShiftClient()
-conn = client.connect()
-cur = conn.cursor()
-conn.autocommit = True   # conn.autocommit True로 설정
-sql = load_query("bi/location_bi.sql")
-cur.execute(sql)
+def create_location_view():
+    client = RedShiftClient()
+    conn = client.connect()
+    conn.autocommit = True   # conn.autocommit True로 설정
+    cur = conn.cursor()
+    
+    try:
+        sql = load_query("bi/location_bi.sql")
+        cur.execute(sql)
+        print("View(location_view) created successfully!")
+    except Exception as e:
+        print("Error while creating View(location_view)", e)
+    finally:
+        cur.close()
+        conn.close()
 
-
-
-
-# sql = load_query("adhoc/test.sql")
-# sql = "select * from public.location_view limit 10;"
-# sql = "SELECT f.event_id,f.event_name,dc.category,dp.pay_type FROM public.fact_event f LEFT JOIN public.dim_category dc ON f.category_key = dc.category_key LEFT JOIN public.dim_payment dp ON f.payment_key = dp.payment_key"
-# sql = "create table public.test ( id INT PRIMARY KEY)"
-# cur.execute("SELECT tablename, COUNT(*) AS cnt FROM pg_tables WHERE schemaname = current_schema() GROUP BY tablename HAVING COUNT(*) > 0;")
-# cur.execute("SELECT grantee,privilege_type FROM information_schema.schema_privileges WHERE grantee = 'dongyoung' AND schema_name = 'public';")
-# cur.execute("SELECT tablename FROM pg_table_def WHERE schemaname = 'public';")
-# cur.execute(sql)
-# print(cur.fetchall())
-# tables = [row[0] for row in cur.fetchall()]
-# print(tables)
+if __name__ == "__main__":
+    create_location_view()
+    
