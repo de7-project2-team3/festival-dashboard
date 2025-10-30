711ddc226a095b9661c94a79a8974409e78cfa63
aneoseo <aneoseo14@gmail.com>
2025-10-30 20:40:02 +0900

    feat: 행사유형별 뷰 수정 (#13)

--- queries/bi/category_view.sql
+++ queries/bi/category_view.sql
@@ -7,16 +7,9 @@ SELECT
     c.category,
     sd.full_date AS start_date,
     ed.full_date AS end_date,
-    TO_CHAR(sd.full_date, 'YYYY-MM') AS start_month,
-    TO_CHAR(ed.full_date, 'YYYY-MM') AS end_month,
-    s.subject_type,
-    l.place_name,
-    l.area,
-    l.latitude,
-    l.longitude
+    s.subject_type
 FROM public.fact_event f
 LEFT JOIN public.dim_category c ON f.category_key = c.category_key
 LEFT JOIN public.dim_date sd ON f.start_date_key = sd.date_key
 LEFT JOIN public.dim_date ed ON f.end_date_key = ed.date_key
-LEFT JOIN public.dim_subject s ON f.subject_key = s.subject_key
-LEFT JOIN public.dim_location l ON f.location_key = l.location_key;
+LEFT JOIN public.dim_subject s ON f.subject_key = s.subject_key;
